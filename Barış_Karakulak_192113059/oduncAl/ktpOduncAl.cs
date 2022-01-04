fa85522662748ac258cba79952d286852a36a8d7
Renklikup <baris.1907.krklk@gmail.com>
2022-01-04 10:48:07 +0300

    comment line 0.7

--- Barış_Karakulak_192113059/oduncAl/ktpOduncAl.cs
+++ Barış_Karakulak_192113059/oduncAl/ktpOduncAl.cs
@@ -34,26 +34,35 @@ namespace Barış_Karakulak_192113059.oduncAl
         }
         private void ktpOduncAl_Load(object sender, EventArgs e)
         {
-            //grid_listele
+            //grid_listele gridine bookcontext gelen getbooks fonksiyonunu atıyorum
             grid_listele.DataSource = bookContext.GetBooks();
+            //radio_ad_gore checked özelligini aktiflestiriyorum
             radio_ad_gore.Checked = true;
+            //btn_odunc_al enabled özelligini deaktiflestiriyorum
             btn_odunc_al.Enabled = false;
+            //txt_id enabled ozelligini deaktiflestiriyorum
             txt_id.Enabled = false;
+            //txt_adi enabled ozelligini deaktiflestiriyorum
             txt_adi.Enabled = false;
+            //btn_uzat enabled ozelligini deaktiflestiriyorum
             btn_uzat.Enabled = false;
+            //txt_soyad enabled ozelligini deaktiflestiriyorum
             txt_soyad.Enabled = false;
 
         }
+        //bookcontext class ından bookcontext isminde bir obje oluşturuyorum
         BookContext bookContext = new BookContext();
         private void btn_ara_Click(object sender, EventArgs e)
         {
-           
+           //radio_ad_gore checked özelligi true ise
             if (radio_ad_gore.Checked)
             {
+                //grid_listele gridine bookcontext goreAra fonksiyonunu atıyorum
                 grid_listele.DataSource = bookContext.GoreAra(txt_aranan_deger.Text, "Book_name");
             }
             else
             {
+                //grid_listele gridine bookcontext goreAra fonksiyonunu atıyorum
                 grid_listele.DataSource = bookContext.GoreAra(txt_aranan_deger.Text, "Book_author");
 
             }
@@ -61,13 +70,16 @@ namespace Barış_Karakulak_192113059.oduncAl
 
         private void txt_aranan_deger_TextChanged(object sender, EventArgs e)
         {
+            //txt_aranan_deger degeri boş ise
             if(txt_aranan_deger.Text=="")
             {
+                //grid_listele gridine bookcontext getbooks fonksiyonunu atıyorum
                 grid_listele.DataSource = bookContext.GetBooks();
 
             }
 
         }
+        //librarycontext sınıfından librarycontext objesi oluşturuyorum
         LibraryContext LibraryContext = new LibraryContext();
         private void grid_listele_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -76,87 +88,121 @@ namespace Barış_Karakulak_192113059.oduncAl
 
         private void btn_odunc_al_Click(object sender, EventArgs e)
         {
+            //grid_listele gridinin seçilen öğesi false ise
             if (!bool.Parse(grid_listele.SelectedRows[0].Cells[5].Value.ToString()))
             {
+                //messagebox yazdır
                 MessageBox.Show("Bu kitap alınmış");
+                //sonlandır
                 return;
             }
+            //integer tipinde bookId isminde bir degisken oluşturdum
+            //bookId degiskeninine grid_listele gridinin secilen degerini atıyorum
             int bookId= int.Parse(grid_listele.SelectedRows[0].Cells[0].Value.ToString());
+            //integer tipinde StudentId isminde bir degisken oluşturdum
+            //StudentId degiskeninine txt_id degerini atıyorum
             int StudentId =int.Parse(txt_id.Text);
+            //Tuple<bool,string> tipinde response adında bir degisken oluşturuyorum
             Tuple<bool, string> response;
+            //Tuple<bool,string> tipinde response2 adında bir degisken oluşturuyorum
             Tuple<bool, string> response2;
-
+            //response degiskenine librarycontext objesinden insertlibrarycontext fonksiyonunu atıyorum
             response = LibraryContext.InsertLibraryContext(bookId, StudentId);
+            //response2 degiskenine bookcontext objesinden oduncAl fonksiyonunu atıyorum
             response2 = bookContext.oduncAl(bookId);
+            //eger response.Item1 ve response.Item1 degerleri true ise
             if (response.Item1 && response2.Item1)
             {
+                //messagebox yazdır
                 MessageBox.Show("Ödünç Alındı");
             }
 
-
+            //integer tipinde ogr_id adında bir degisken oluşturuyorum
+            //ogr_id degiskeninie txt_id den gelen degeri atıyorum
             int ogr_id = int.Parse(txt_id.Text);
+            //grid_listele gridine bookcontext den gelen getbooks fonksiyonunu atıyorum
             grid_listele.DataSource = bookContext.GetBooks();
+            //grid_odunc_alinan_ktp gridine librarycontext objesinden gelen teslimEtmedigiKitaplar fonksiyounun atıyorum
             grid_odunc_alinan_ktp.DataSource = LibraryContext.teslimEtmedigiKitaplar(ogr_id);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //ogrSec formundan ogrSec adında bir obje oluşturuyorum
             ogrSec ogrSec = new ogrSec(this);
+            //ogrSec objesini gosteriyorum
             ogrSec.ShowDialog();
             
         }
 
         private void closelabel_Click(object sender, EventArgs e)
         {
+            //formu kapat
             this.Close();
         }
-        LibraryProvider LibraryProvider = new LibraryProvider();
+        
         private void grid_listele_CellClick_1(object sender, DataGridViewCellEventArgs e)
         {
+            //grid_listele secilen satır sayısı 0 dan büyük ve txt_id degeri bos degil ise
             if (grid_listele.SelectedRows.Count > 0 && txt_id.Text!="")
             {
-                
+                //btn_odunc_al enabled ozelligini aktiflestiriyorum
                 btn_odunc_al.Enabled = true;
                
             }
             else
             {
+                //btn_odunc_al enabled ozelligini deaktiflestiriyorum
                 btn_odunc_al.Enabled = false;
             }
         }
 
         private void txt_id_TextChanged(object sender, EventArgs e)
         {
+            //integer tipinde ogr_id isminde txt_id degerini atıyorum
             int ogr_id = int.Parse(txt_id.Text);
+            //grid_odunc_alinan_ktp gridini librarycontext objesinden gelen teslimEtmedigiKitaplar fonksiyonunu atıyorum 
             grid_odunc_alinan_ktp.DataSource = LibraryContext.teslimEtmedigiKitaplar(ogr_id);
 
         }
 
         private void btn_uzat_Click(object sender, EventArgs e)
         {
+            //eger grid_odunc_alinan_ktp nin secilen kolonların sayısı 0 dan büyükse
             if(grid_odunc_alinan_ktp.SelectedRows.Count>0)
             {
+                //integer tipinde id adında bir degisken oluşturdum
+                //id degiskenine grid_odunc_alinan_ktp secilen degerini atıyorum
                 int id =int.Parse(grid_odunc_alinan_ktp.SelectedRows[0].Cells[0].Value.ToString());
+                //tuple<bool,string> tipinde response adında bir degisken olusturuyorum
                 Tuple<bool, string> response;
-                response=LibraryContext.extendExpiryDateContext(id);
-                
+                //repsonse degiskenine extendExpiryDateContext fonksiyonunu atıyorum
+                response = LibraryContext.extendExpiryDateContext(id);
+                //response.Item1 degeri true ise
                 if (response.Item1)
                 {
+                    //messagebox yazdır
                     MessageBox.Show("İade Edildi");
                 }
+                //integer tipine ogr_id adında bir degisken olusturuyorum
+                //ogr_id degiskenine txt_id den gelen degeri atıyorum
                 int ogr_id = int.Parse(txt_id.Text);
+                //grid_odunc_alinan_ktp gridine teslimEtmedigiKitaplar fonksiyonunu atıyorum
                 grid_odunc_alinan_ktp.DataSource = LibraryContext.teslimEtmedigiKitaplar(ogr_id);
             }
         }
 
         private void grid_odunc_alinan_ktp_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            //grid_odunc_alinan_ktp secilen kolon sayısı 0 dan buyukse
             if (grid_odunc_alinan_ktp.SelectedRows.Count > 0)
             {
+                //btn_uzat enable ozelligini aktiflestiriyorum
                 btn_uzat.Enabled = true;
             }
             else
             {
+                //btn_uzat enable ozelligini deaktiflestiriyorum
                 btn_uzat.Enabled = false;
             }
         }
--- Barış_Karakulak_192113059/oduncAl/ogrSec.cs
+++ Barış_Karakulak_192113059/oduncAl/ogrSec.cs
@@ -13,19 +13,24 @@ namespace Barış_Karakulak_192113059.oduncAl
 {
     public partial class ogrSec : Form
     {
-       
+       //ktpOduncAl formunda form adında bir obje oluşturdum
         ktpOduncAl form;
+        //ogrSec Constructerı
         public ogrSec(ktpOduncAl form)
         {
             InitializeComponent();
+            //contructer parametresini this.form a atıyorum
             this.form = form;
         }
 
         
         private void ogrSec_Load(object sender, EventArgs e)
         {
+            //btn_sec enabled ozelligini deaktiflestiriyorum
             btn_sec.Enabled = false;
+            //radio_ad_gore checked ozelligini aktiflestiriyorum
             radio_ad_gore.Checked = true;
+            //data_grid_listele gridine GetStudents fonksiyonunu atıyoruö
             data_grid_listele.DataSource = StudentContext.GetStudents();
         }
         protected override void WndProc(ref Message m)
@@ -43,54 +48,74 @@ namespace Barış_Karakulak_192113059.oduncAl
         }
         private void closelabel_Click(object sender, EventArgs e)
         {
+            //formu kapatıyorum
             this.Close();
         }
 
         private void data_grid_listele_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            //data_grid_listele secilen satır sayısı 0 dan buyukse
             if (data_grid_listele.SelectedRows.Count > 0)
             {
+                //btn_sec enabled aktiflestiriyorum
                 btn_sec.Enabled = true;
             }
             else
             {
+                //btn_sec enabled deaktiflestiriyorum
                 btn_sec.Enabled = false;
             }
         }
+        //studentcontext classından studentcontext objesi oluşturdum
         StudentContext StudentContext = new StudentContext();
         private void btn_ara_Click(object sender, EventArgs e)
         {
+            //eger txt_aranan_deger bos degil ise
             if (txt_aranan_deger.Text != "")
             {
+                //string tipinde aranacakDeger adında bir degisken oluşturdum
+                //arancakDeger degerine txt_aranan_deger degerini atıyorum
                 string aranacakDeger = txt_aranan_deger.Text;
+                //radio_ad_gore checked ozelligi true ise
                 if (radio_ad_gore.Checked)
                 {
 
-
+                    //data_grid_listele gridine goreAra fonksiyonu atıyorum
                     data_grid_listele.DataSource = StudentContext.GoreAra(aranacakDeger, "Student_name");
                 }
+                //eger radio_soyad_gore checked ozelligi true ise
                 else if (radio_soyad_gore.Checked)
                 {
+                    //data_grid_listele gridine goreAra fonksiyonunu atıyorum
                     data_grid_listele.DataSource = StudentContext.GoreAra(aranacakDeger, "Student_surname");
 
                 }
             }
             else
             {
+                //messagebox yazdır
                 MessageBox.Show("Aranacak Alanı Doldurunuz");
             }
         }
 
         private void btn_sec_Click(object sender, EventArgs e)
         {
+            //string tipinde id adında bir degisken oluşturdum
+            //id degiskenine data_grid_listele secilen kolonun degerini atıyorum
             string id = data_grid_listele.SelectedRows[0].Cells[0].Value.ToString();
+            //string tipinde name adında bir degisken oluşturdum
+            //name degiskenine data_grid_listele secilen kolonun degerini atıyorum
             string name = data_grid_listele.SelectedRows[0].Cells[1].Value.ToString();
+            //string tipinde surname adında bir degisken oluşturdum
+            //surname degiskenine data_grid_listele secilen kolonun degerini atıyorum
             string surname = data_grid_listele.SelectedRows[0].Cells[2].Value.ToString();
-
+            //form.txt_id textbox ına id degerini atıyorum
             form.txt_id.Text = id;
+            //form.txt_adi textbox ına name degerini atıyorum
             form.txt_adi.Text = name;
+            //form.txt_soyad textbox ına surname degerini atıyorum
             form.txt_soyad.Text = surname;
+            //formu kapat
             this.Close();
         }
     }
